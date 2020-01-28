2b1fa6743a5b2b70ab585a1568513ccf8009b4f9
Crystal Soo Lee <soo.leesj@gmail.com>
2020-01-28 15:37:05 -0800

    followed the demo

---
+++ src/main/java/com/example/codefellowship/controlloers/ApplicationUserController.java
@@ -0,0 +1,38 @@
+package com.example.codefellowship.controlloers;
+
+import com.example.codefellowship.models.ApplicationUser;
+import com.example.codefellowship.models.ApplicationUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+public class ApplicationUserController {
+    @Autowired
+    ApplicationUserRepository applicationUserRepository;
+
+    @Autowired private PasswordEncoder passwordEncoder;
+
+    @PostMapping("/signup")
+    public RedirectView createNewApplicationUser(String username, String password){
+        System.out.println("You are adding a user");
+        // make the user AND salt and hash the password
+        // this does the salting and hashing for you
+        ApplicationUser newUser = new ApplicationUser(username, passwordEncoder.encode(password));
+
+        // save the user to db
+        applicationUserRepository.save(newUser);
+
+        // send them back home
+        return new RedirectView("/");
+    }
+
+    @GetMapping("/login")
+    public String showLoginForm(){
+        return "login";
+    }
+
+}
